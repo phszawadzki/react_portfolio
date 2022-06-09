b50ad5dbcd62021bed37ce45831958de036cb8a0
phszawadzki <phszawadzki@gmail.com>
2022-06-09 21:53:58 +0200

    Updates

---
+++ precache-manifest.cab5b8b01ff04c4c2a959b2303027d4b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "028154e7e8e08a577154",
+    "url": "./static/css/main.1207231f.chunk.css"
+  },
+  {
+    "revision": "028154e7e8e08a577154",
+    "url": "./static/js/main.028154e7.chunk.js"
+  },
+  {
+    "revision": "56ca727cfce41073dd60",
+    "url": "./static/js/1.56ca727c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "./static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "./static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "./static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "9a878e1ff19047763c03c1aa5a58c1e8",
+    "url": "./static/media/avatar_radek_marciniak.9a878e1f.png"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "./static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "./static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "./static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "./static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "./static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "ad207768b113e00dd33b87676d44ead2",
+    "url": "./index.html"
+  }
+];
